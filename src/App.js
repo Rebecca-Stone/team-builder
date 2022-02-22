591aa001c3da567220bec1fa69bb5124c05ca8cd
Rebecca Stone <you@example.com>
2022-02-22 17:46:13 -0600

    created basic form that renders, will now work on making it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+//importing React and useState to create slices of state
+import React, { useState } from 'react';
+//importing form to be able to carry over data
+import Form from './components/Form'; 
+
+const initialState = {
+  name: '',
+  email: '',
+  role: ','
+}
 
 function App() {
+  const [team, setTeamMembers] = useState([]);
+  const [values, setValues] = useState(initialState);
+
+  const onSubmit = () => {
+    setTeamMembers({values, ...team});
+    setValues(initialState);
+  }
+
+  const onChange = (name, value) => {
+    setValues({...values, [name]: value})
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Join Us!</h1>
+      <Form
+        values={values}
+        change={onChange}
+        submit={onSubmit}
+      />
+      {team.map((team, index) => {
+        return (
+          <div key={index}>
+            {team.name}, {team.role}, {team.email}
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
 
 export default App;
